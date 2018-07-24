dc543598ed4a357d190e9fb004087176e9d7b5c1
Filip Kucharczyk <fku-ext@danskecommodities.com>
2018-07-24 07:40:00 +0200

    handle for C

--- src/test/java/com/company/DiamondTest.java
+++ src/test/java/com/company/DiamondTest.java
@@ -12,8 +12,17 @@ public class DiamondTest {
     @Test
     public void when_input_is_B_returns_AB() {
         String expected =
-                        "A\n" +
-                        " B";
+                "A\n" +
+                " B";
         Assert.assertEquals(expected, Diamond.printDiamond('B'));
+    }
+
+    @Test
+    public void when_input_is_letter_returns_one_diamond_edge_with_all_letters_until_the_input_teller() {
+        String expected =
+                "A\n" +
+                " B\n" +
+                "  C";
+        Assert.assertEquals(expected, Diamond.printDiamond('C'));
     }
 }
