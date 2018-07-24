16c6a197569c01e14be562d733285b2c354e7757
Filip Kucharczyk <fku-ext@danskecommodities.com>
2018-07-24 07:45:45 +0200

    refactor - extract methods

--- src/main/java/com/company/Diamond.java
+++ src/main/java/com/company/Diamond.java
@@ -6,12 +6,23 @@ public class Diamond {
         int howManyLinesToPrint = letter - 'A' + 1;
         String result = "";
         for (int i = 0; i < howManyLinesToPrint; i++) {
-            for (int j = 0; j < i; j++) {
-                result += " ";
-            }
+            result += spacesRepeatedTimes(i);
             result += String.valueOf((char)('A' + i));
             result += "\n";
         }
+        result = removeLastNewLineCharacter(result);
+        return result;
+    }
+
+    private static String spacesRepeatedTimes(int i) {
+        String spaces = "";
+        for (int j = 0; j < i; j++) {
+            spaces += " ";
+        }
+        return spaces;
+    }
+
+    private static String removeLastNewLineCharacter(String result) {
         result = result.substring(0, result.lastIndexOf('\n'));
         return result;
     }
