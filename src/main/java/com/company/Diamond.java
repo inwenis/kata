67876643f93fe1e9d0918c20151e355db9630154
Filip Kucharczyk <fku-ext@danskecommodities.com>
2018-07-24 07:42:24 +0200

    refactor - the last loop handles all cases

--- src/main/java/com/company/Diamond.java
+++ src/main/java/com/company/Diamond.java
@@ -5,22 +5,16 @@ import java.util.Enumeration;
 public class Diamond {
 
     public static String printDiamond(char letter) {
-        if(letter == 'A') {
-            return "A";
-        } else if (letter == 'B'){
-            return "A\n B";
-        } else {
-            int howManyLinesToPrint = letter - 'A' + 1;
-            String result = "";
-            for (int i = 0; i < howManyLinesToPrint; i++) {
-                for (int j = 0; j < i; j++) {
-                    result += " ";
-                }
-                result += String.valueOf((char)('A' + i));
-                result += "\n";
+        int howManyLinesToPrint = letter - 'A' + 1;
+        String result = "";
+        for (int i = 0; i < howManyLinesToPrint; i++) {
+            for (int j = 0; j < i; j++) {
+                result += " ";
             }
-            result = result.substring(0, result.lastIndexOf('\n'));
-            return result;
+            result += String.valueOf((char)('A' + i));
+            result += "\n";
         }
+        result = result.substring(0, result.lastIndexOf('\n'));
+        return result;
     }
 }
