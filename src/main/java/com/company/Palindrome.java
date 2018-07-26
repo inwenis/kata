fd169815c9a50930289938853b2fa080c50f60de
Filip Kucharczyk <fku-ext@danskecommodities.com>
2018-07-26 09:28:45 +0200

    refactor

--- src/main/java/com/company/Palindrome.java
+++ src/main/java/com/company/Palindrome.java
@@ -8,12 +8,8 @@ public class Palindrome {
     public static boolean isPalindrome(String input) {
         if(input.equals("")) {
             return false;
-        } else if(input.length() == 1) {
-            return true;
-        } else if (input.length() == 2) {
-            return input.charAt(0) == input.charAt(1);
         } else {
-            return input.charAt(0) == input.charAt(2);
+            return input.charAt(0) == input.charAt(input.length() - 1);
         }
     }
 }
