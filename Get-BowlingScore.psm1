ae351ce092796907cdc5d3bfdbd025a4b66b5e51
Filip Kucharczyk <fku-ext@danskecommodities.com>
2018-07-29 15:12:38 +0200

    score for 10 frames without spare and strike is sum of all pins from all frames

--- Get-BowlingScore.psm1
+++ Get-BowlingScore.psm1
@@ -1,9 +1,16 @@
 function Get-BowlingScore {
     param([string]$frames)
     #write-host $frames
-    if($frames.Contains("1")) {
+    if ($frames.Contains("1-1")) {
         return 10
-    } else {
+    } elseif ($frames.Contains("0")) {
         return 0
+    } else {
+        [int]$score = 0
+        for ($i = 0; $i -lt $frames.Length; $i+=2) {
+            #write-host $frames[$i] " " $score
+            $score += [int][string]$frames[$i]
+        }
+        return $score
     }
 }
