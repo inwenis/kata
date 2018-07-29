9f7207299e3bdc404c68942cb424c60c28f09b18
Filip Kucharczyk <fku-ext@danskecommodities.com>
2018-07-29 15:13:46 +0200

    refactor - remove redundant code

--- Get-BowlingScore.psm1
+++ Get-BowlingScore.psm1
@@ -1,16 +1,9 @@
 function Get-BowlingScore {
     param([string]$frames)
-    #write-host $frames
-    if ($frames.Contains("1-1")) {
-        return 10
-    } elseif ($frames.Contains("0")) {
-        return 0
-    } else {
-        [int]$score = 0
-        for ($i = 0; $i -lt $frames.Length; $i+=2) {
-            #write-host $frames[$i] " " $score
-            $score += [int][string]$frames[$i]
-        }
-        return $score
+    [int]$score = 0
+    for ($i = 0; $i -lt $frames.Length; $i+=2) {
+        #write-host $frames[$i] " " $score
+        $score += [int][string]$frames[$i]
     }
+    return $score
 }
