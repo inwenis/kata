3062a98189e064a5245107b26afd6716d0694a9e
Filip Kucharczyk <fku-ext@danskecommodities.com>
2018-07-29 15:17:43 +0200

    count pins from second try

--- BowlingScoreTests.ps1
+++ BowlingScoreTests.ps1
@@ -13,3 +13,4 @@ function test {
 test -expected 10 -testInput "1-1-1-1-1-1-1-1-1-1-" -message "10 frames with score 1 should return 10"
 test 0 "0-0-0-0-0-0-0-0-0-0-" "10 frames with score 0 should return 0"
 test 54 "1-2-3-4-5-6-7-8-9-9-" "score for frames without strikes and spares returns sum of pins in all frames"
+test 55 "112-3-4-5-6-7-8-9-9-" "score for frames without strikes and spares returns sum of pins in all frames and pins from second try are counted also"
--- Get-BowlingScore.psm1
+++ Get-BowlingScore.psm1
@@ -1,9 +1,11 @@
 function Get-BowlingScore {
     param([string]$frames)
     [int]$score = 0
-    for ($i = 0; $i -lt $frames.Length; $i+=2) {
+    for ($i = 0; $i -lt $frames.Length; $i+=1) {
         #write-host $frames[$i] " " $score
-        $score += [int][string]$frames[$i]
+        if(-not($frames[$i] -eq '-')) {
+            $score += [int][string]$frames[$i]
+        }
     }
     return $score
 }
