9c90c595aba0df48dfbfd08bdfb1b44e0001dbbe
Filip Kucharczyk <fku-ext@danskecommodities.com>
2018-07-29 20:16:29 +0200

    a miss is a dash, not 0

--- BowlingScoreTests.ps1
+++ BowlingScoreTests.ps1
@@ -14,4 +14,5 @@ test -expected 10 -testInput "1-1-1-1-1-1-1-1-1-1-" -message "10 frames with sco
 test 0 "0-0-0-0-0-0-0-0-0-0-" "10 frames with score 0 should return 0"
 test 54 "1-2-3-4-5-6-7-8-9-9-" "score for frames without strikes and spares returns sum of pins in all frames"
 test 55 "112-3-4-5-6-7-8-9-9-" "score for frames without strikes and spares returns sum of pins in all frames and pins from second try are counted also"
-test 10 "0/0-0-0-0-0-0-0-0-0-" "a spare equals 10 pins in a frame"
+test 10 "2/0-0-0-0-0-0-0-0-0-" "a spare equals 10 pins in a frame"
+test 10 "-/0-0-0-0-0-0-0-0-0-" "a spare equals 10 pins in a frame"
--- Get-BowlingScore.psm1
+++ Get-BowlingScore.psm1
@@ -1,15 +1,22 @@
 function Get-BowlingScore {
     param([string]$frames)
+
     [int]$score = 0
+
     for ($i = 0; $i -lt $frames.Length; $i+=1) {
-        #write-host $frames[$i] " " $score
         if($frames[$i] -eq '-') {
             #do nothing
         } elseif ($frames[$i] -eq '/') {
-            $score += 10 - [int][string]$frames[$i-1]
+            if($frames[$i-1] -eq '-') {
+                $score += 10
+            } else {
+                $score += 10 - [int][string]$frames[$i-1]
+            }
+
         } else {
             $score += [int][string]$frames[$i]
         }
     }
+
     return $score
 }
