8e50830dca0faa39771ac1982bbf79bde38e948b
Filip Kucharczyk <fku-ext@danskecommodities.com>
2018-07-30 08:16:34 +0200

    refactor - simplify solution

--- Get-BowlingScore.psm1
+++ Get-BowlingScore.psm1
@@ -1,78 +1,42 @@
 function Get-BowlingScore {
     param([string]$framesAsText)
 
-    [int]$score = 0
-
-    $frames = @()
-    for ($i = 0; $i -lt 20; $i+=2) {
-        $frames += @($framesAsText.substring($i, 2))
-    }
-
-    if ($framesAsText[19] -eq "/") {
-        $frames += @($framesAsText.substring(20, 1) + " ")
-    } elseif ($framesAsText[18] -eq "X") {
-        $frames += @($framesAsText.substring(20, 2))
-        if ($framesAsText[20] -eq "X") {
-            $frames += @($framesAsText.substring(22, 2))
-        }
-    }
-
     function symbolToPinsDown { # aka. how many pins down
-        param([char]$symbol)
+        param([string]$framesAsText, [int]$index)
+        $symbol = $framesAsText[$index]
         if ($symbol -eq '-') {
             return 0
         } elseif ($symbol -eq 'X') {
             return 10
+        } elseif ($symbol -eq '/') {
+            return 10 - (symbolToPinsDown $framesAsText ($index - 1))
         }
         else {
             return [int][string]$symbol
         }
     }
-    function scoreForFrame {
-        param([string]$frame, [string]$nextFrame, [string]$nextNextFrame)
 
-        if ($frame -match "[0-9-]{2}") {
-            $sum += symbolToPinsDown $frame[0]
-            $sum += symbolToPinsDown $frame[1]
-        } elseif ($frame -match "[0-9-]/") {
-            $sum += 10
-            $sum += symbolToPinsDown $nextFrame[0]
-        } elseif ($frame -match "X ") {
-            $sum += 10
-            if ($nextFrame -match "[0-9-]{2}") {
-                $sum += symbolToPinsDown $nextFrame[0]
-                $sum += symbolToPinsDown $nextFrame[1]
-            } elseif ($nextFrame -match "[0-9-]/") {
-                $sum += 10
-            } elseif ($nextFrame -match "X ") {
-                $sum += 10
-                $sum += symbolToPinsDown $nextNextFrame[0]
-            } else {
-                throw "this should not happen"
-            }
+    [int] $sum = 0
+    [int] $index = 0
+    foreach ($frame in @(1..10)) {
+        if ($framesAsText.Substring($index, 2) -match "[0-9-]{2}") {
+            $sum += symbolToPinsDown $framesAsText ($index + 0) # pins down
+            $sum += symbolToPinsDown $framesAsText ($index + 1) # pins down
+            $index += 2
+        } elseif ($framesAsText.Substring($index, 2) -match "[0-9-]/") {
+            $sum += symbolToPinsDown $framesAsText ($index + 0) # pins down
+            $sum += symbolToPinsDown $framesAsText ($index + 1) # pins down
+            $sum += symbolToPinsDown $framesAsText ($index + 2) # bonus
+            $index += 2
+        } elseif ($framesAsText[$index] -eq 'X') {
+            $sum += symbolToPinsDown $framesAsText ($index + 0) # pins down
+            $sum += symbolToPinsDown $framesAsText ($index + 1) # bonus
+            $sum += symbolToPinsDown $framesAsText ($index + 2) # bonus
+            $index += 1
         } else {
             throw "this should not happen"
         }
-        return $sum
     }
 
-    foreach ($frameIndex in @(0..7)) {
-        $score += scoreForFrame $frames[$frameIndex] $frames[$frameIndex + 1] $frames[$frameIndex + 2]
-    }
-    
-    if($frames.Length -gt 10) {
-        $score += scoreForFrame $frames[8] $frames[9] $frames[10]
-    } else {
-        $score += scoreForFrame $frames[8] $frames[9] $null
-    }
-    
-    if($frames.Length -eq 12) {
-        $score += scoreForFrame $frames[9] $frames[10] $frames[11]
-    } elseif($frames.Length -eq 11) {
-        $score += scoreForFrame $frames[9] $frames[10] $null
-    } else {
-        $score += scoreForFrame $frames[9] $null $null
-    }
-
-    return $score
+    return $sum
 }
