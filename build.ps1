47b2c6a6ad270861d29827182048b36780f33f02
Filip Kucharczyk <fku-ext@danskecommodities.com>
2018-07-31 08:07:31 +0200

    update build and ignore build results

--- build.ps1
+++ build.ps1
@@ -1,4 +1,10 @@
 if(-not(Test-Path "packages\nunit*")) {
     nuget install nunit -outputdirectory packages
 }
-csc *.cs /r:packages\NUnit.3.10.1\lib\net45\nunit.framework.dll
+
+if(-not(Test-Path "out")) {
+    mkdir "out"
+}
+
+csc Game.cs /t:library /out:out\Game.dll
+csc GameTests.cs /t:library /r:packages\NUnit.3.10.1\lib\net45\nunit.framework.dll,out\Game.dll /out:out\GameTests.dll
