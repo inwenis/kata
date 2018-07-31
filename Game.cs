e196621e4d96b5afa2c243616742abf64396a64f
Filip Kucharczyk <fku-ext@danskecommodities.com>
2018-07-31 08:41:41 +0200

    refactor - extract method

--- Game.cs
+++ Game.cs
@@ -14,7 +14,7 @@ public class Game
         int rollIndex = 0;
         for(int frame = 0; frame < 10 ; frame++)
         {
-            if(_rolls[rollIndex] + _rolls[rollIndex+1] == 10)
+            if(IsSpare(_rolls[rollIndex], _rolls[rollIndex+1]))
             {
                 score += 10;
                 score += _rolls[rollIndex+2];
@@ -28,5 +28,10 @@ public class Game
             rollIndex += 2;
         }
         return score;
+    }
+
+    public bool IsSpare(int roll1, int roll2)
+    {
+        return roll1 + roll2 == 10;
     }
 }
