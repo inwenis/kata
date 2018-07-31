5f70fb73c3c9fecca10eedc61b65833813c4d32f
Filip Kucharczyk <fku-ext@danskecommodities.com>
2018-07-31 09:04:37 +0200

    refactor - extract methods

--- Game.cs
+++ Game.cs
@@ -17,33 +17,46 @@ public class Game
             if (IsSpare(_rolls[rollIndex], _rolls[rollIndex+1]))
             {
                 score += 10;
-                score += _rolls[rollIndex+2];
+                score += SpareBonus(rollIndex);
                 rollIndex += 2;
             }
             else if (IsStrike(_rolls[rollIndex])) //strike
             {
                 score += 10;
-                score += _rolls[rollIndex+1];
-                score += _rolls[rollIndex+2];
+                score += StrikeBonus(rollIndex);
                 rollIndex += 1;
             }
             else
             {
-                score += _rolls[rollIndex];
-                score += _rolls[rollIndex+1];
+                score += SumPinsInFrame(rollIndex);
                 rollIndex += 2;
             }
         }
         return score;
     }
 
-    public bool IsSpare(int roll1, int roll2)
+    private bool IsSpare(int roll1, int roll2)
     {
         return roll1 + roll2 == 10;
     }
 
-    public bool IsStrike(int roll)
+    private bool IsStrike(int roll)
     {
         return roll == 10;
+    }
+
+    private int SpareBonus(int rollIndex)
+    {
+        return _rolls[rollIndex+2];
+    }
+
+    private int StrikeBonus(int rollIndex)
+    {
+        return _rolls[rollIndex+1] + _rolls[rollIndex+2];
+    }
+
+    private int SumPinsInFrame(int rollIndex)
+    {
+        return _rolls[rollIndex] + _rolls[rollIndex+1];
     }
 }
