217c99f451b3c174df3aaf67beba4a8248dc9ec9
Filip Kucharczyk <fku-ext@danskecommodities.com>
2018-07-31 08:22:05 +0200

    handle score for all rolls knock down 1 pin

--- Game.cs
+++ Game.cs
@@ -1,12 +1,14 @@
 public class Game
 {
+    int _score = 0;
+
     public void Roll(int pinsDown)
     {
-
+        _score += pinsDown;
     }
 
     public int Score()
     {
-        return 0;
+        return _score;
     }
 }
--- GameTests.cs
+++ GameTests.cs
@@ -12,4 +12,14 @@ public class GameTests
         }
         Assert.AreEqual(0, sut.Score());
     }
+
+    [Test]
+    public void when_all_rolls_have_1_pin_down_score_is_20()
+    {
+        Game sut = new Game();
+        for(int i = 0; i < 20 ; i++) {
+            sut.Roll(1);
+        }
+        Assert.AreEqual(20, sut.Score());
+    }
 }
