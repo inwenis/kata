a862dc4c82968b346fe1d0b8e61d0aab3693e67c
Filip Kucharczyk <fku-ext@danskecommodities.com>
2018-07-31 08:26:51 +0200

    refactor tests - remove duplication

--- GameTests.cs
+++ GameTests.cs
@@ -3,23 +3,33 @@ using NUnit.Framework;
 [TestFixture]
 public class GameTests
 {
+    Game sut;
+
+    [SetUp]
+    public void Init()
+    {
+        sut = new Game();
+    }
+
     [Test]
     public void when_all_rolls_have_0_pins_down_score_is_0()
     {
-        Game sut = new Game();
-        for(int i = 0; i < 20 ; i++) {
-            sut.Roll(0);
-        }
+        RollMany(20, 0);
         Assert.AreEqual(0, sut.Score());
     }
 
     [Test]
     public void when_all_rolls_have_1_pin_down_score_is_20()
     {
-        Game sut = new Game();
-        for(int i = 0; i < 20 ; i++) {
-            sut.Roll(1);
-        }
+        RollMany(20, 1);
         Assert.AreEqual(20, sut.Score());
+    }
+
+    public void RollMany(int rolls, int pinsDown)
+    {
+        for(int i = 0; i < rolls ; i++)
+        {
+            sut.Roll(pinsDown);
+        }
     }
 }
