ba9ac7b7eb787e614d857227c5abc35940660730
Filip Kucharczyk <fku-ext@danskecommodities.com>
2018-09-23 20:28:50 +0200

    use supplied wordlist for tests

---
+++ anagram_kata/Anagramalist.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace anagram_kata
+{
+    public class Anagramalist
+    {
+        private string[] _words;
+
+        public Anagramalist(string[] words)
+        {
+            _words = words;
+        }
+
+        public IEnumerable<string> ComputeAll2WordsAnagrams(string anagramSubject)
+        {
+            var wordsWhichCanBeUsedInAnagrams = _words
+                .Where(word => CharactersAreSubsetOf(word, anagramSubject))
+                .ToArray();
+
+            foreach (var firstWord in wordsWhichCanBeUsedInAnagrams.ToArray())
+            {
+                var remainingCharacters = RemoveCharacters(anagramSubject, firstWord);
+
+                var possibleSecondsWords = wordsWhichCanBeUsedInAnagrams
+                    .SkipWhile(x => x != firstWord)
+                    .Where(word => CharactersAreSubsetOf(word, remainingCharacters))
+                    .ToArray();
+
+                foreach (var secondWord in possibleSecondsWords)
+                {
+                    yield return $"{firstWord} {secondWord}";
+                }
+            }
+        }
+
+        private static List<char> RemoveCharacters(string left, string right)
+        {
+            var remainingCharacters = left.ToList();
+            foreach (var character in right)
+            {
+                if (!remainingCharacters.Remove(character))
+                {
+                    var leftRemaining = string.Join("", remainingCharacters);
+                    throw new InvalidOperationException(
+                        $"Can not remove {left} - {right}. Remaining characters are: {leftRemaining}. Could not remove {character}");
+                }
+            }
+
+            return remainingCharacters;
+        }
+
+        private static bool CharactersAreSubsetOf(string setToCheck, string setToCheckAgainst)
+        {
+            return CharactersAreSubsetOf(setToCheck, setToCheckAgainst.ToList());
+        }
+
+        private static bool CharactersAreSubsetOf(string setToCheck, List<char> setToCheckAgainst)
+        {
+            var charactersList = setToCheckAgainst.ToList();
+
+            foreach (var character in setToCheck)
+            {
+                if (!charactersList.Remove(character))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
--- anagram_kata/Program.cs
+++ anagram_kata/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,10 +10,11 @@ namespace anagram_kata
     {
         static void Main(string[] args)
         {
-            var anagramSubject = "documenting";
-
-            var anagramalist = new Anagramalist(new [] { "document", "ing"});
-            var anagrams = anagramalist.ComputeAll2WordsAnagrams(anagramSubject);
+//            var words = new [] { "document", "ing"};
+            var words = ReadFromFile("wordlist.txt");
+            
+            var anagramalist = new Anagramalist(words);
+            var anagrams = anagramalist.ComputeAll2WordsAnagrams("documenting");
             foreach (var anagram in anagrams)
             {
                 Console.WriteLine(anagram);
@@ -22,73 +23,14 @@ namespace anagram_kata
             Console.WriteLine("Press [enter] to exit.");
             Console.ReadLine();
         }
-    }
 
-    public class Anagramalist
-    {
-        private string[] _words;
-
-        public Anagramalist(string[] words)
+        private static string[] ReadFromFile(string fileName)
         {
-            _words = words;
-        }
-
-        public IEnumerable<string> ComputeAll2WordsAnagrams(string anagramSubject)
-        {
-            var wordsWhichCanBeUsedInAnagrams = _words
-                .Where(word => CharactersAreSubsetOf(word, anagramSubject))
-                .ToArray();
-
-            foreach (var firstWord in wordsWhichCanBeUsedInAnagrams.ToArray())
-            {
-                var remainingCharacters = RemoveCharacters(anagramSubject, firstWord);
-
-                var possibleSecondsWords = wordsWhichCanBeUsedInAnagrams
-                    .SkipWhile(x => x != firstWord)
-                    .Where(word => CharactersAreSubsetOf(word, remainingCharacters))
-                    .ToArray();
-
-                foreach (var secondWord in possibleSecondsWords)
-                {
-                    yield return $"{firstWord} {secondWord}";
-                }
-            }
-        }
-
-        private static List<char> RemoveCharacters(string left, string right)
-        {
-            var remainingCharacters = left.ToList();
-            foreach (var character in right)
-            {
-                if (!remainingCharacters.Remove(character))
-                {
-                    var leftRemaining = string.Join("", remainingCharacters);
-                    throw new InvalidOperationException(
-                        $"Can not remove {left} - {right}. Remaining characters are: {leftRemaining}. Could not remove {character}");
-                }
-            }
-
-            return remainingCharacters;
-        }
-
-        private static bool CharactersAreSubsetOf(string setToCheck, string setToCheckAgainst)
-        {
-            return CharactersAreSubsetOf(setToCheck, setToCheckAgainst.ToList());
-        }
-
-        private static bool CharactersAreSubsetOf(string setToCheck, List<char> setToCheckAgainst)
-        {
-            var charactersList = setToCheckAgainst.ToList();
-
-            foreach (var character in setToCheck)
-            {
-                if (!charactersList.Remove(character))
-                {
-                    return false;
-                }
-            }
-
-            return true;
+            var allLines = File.ReadAllLines("wordlist.txt");
+            var withoutHeader = allLines.Skip(1).ToArray();
+            var withoutLast = withoutHeader.Take(withoutHeader.Count() - 1);
+            var words = withoutLast.SelectMany(line => line.Split(new char[] {' '}, StringSplitOptions.RemoveEmptyEntries));
+            return words.ToArray();
         }
     }
 }
