f885e6f284a180276af18c9612671b34d52104ac
Filip Kucharczyk <fku-ext@danskecommodities.com>
2018-09-25 08:52:47 +0200

    refactor

--- anagram_kata2/Program.cs
+++ anagram_kata2/Program.cs
@@ -17,27 +17,12 @@ namespace anagram_kata2
     {
         public static string[] FindAllAnagrams(string[] words)
         {
-            if (!words.Any())
-            {
-                return new string[0];
-            }
-            else if(words.Length == 2)
-            {
-                return new []{words[0] + " " + words[1]};
-            }
-            else if(words.All(x => x.Length == words[0].Length))
-            {
-                return new []{words[0] + " " + words[1] + " " + words[2]};
-            }
-            else
-            {
-                var groupBy = words
-                    .GroupBy(x => x.Length)
-                    .Where(x => x.Count() > 1)
-                    .Select(x => string.Join(" ", x))
-                    .ToArray();
-                return groupBy;
-            }
+            var anagrams = words
+                .GroupBy(word => word.Length)
+                .Where(groupedByLength => groupedByLength.Count() > 1)
+                .Select(x => string.Join(" ", x))
+                .ToArray();
+            return anagrams;
         }
     }
 }
