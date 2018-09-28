84545800a30b2c3c7855c4a5dbfa762a0f34e34f
Filip Kucharczyk <fku-ext@danskecommodities.com>
2018-09-28 21:48:56 +0200

    cleanup

--- anagram_kata2/Anagramalist.cs
+++ anagram_kata2/Anagramalist.cs
@@ -1,58 +1,28 @@
-﻿using System.Collections.Concurrent;
+﻿using System;
+using System.Collections.Concurrent;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace anagram_kata2
 {
-    public class Anagramalist : IAnagramalist
+    public class AnagramalistWithOutSorting : IAnagramalist
     {
         public string[] FindAllAnagrams(string[] words)
         {
-            var i = 20000;
-            var batches = words.Length/i;
-            batches += 1;
+            var dic = new ConcurrentDictionary<int, string>();
 
-            var dic = new ConcurrentDictionary<string, string>();
-
-            Parallel.For(0, batches, (batch) =>
+            var parallelLoopResult = Parallel.ForEach(words, word =>
             {
-                var from = batch * i;
-                var to = (batch + 1) * i;
-                for (int j = from; j < to && j < words.Length; j++)
-                {
-                    string word = words[j];
-                    //var key = KeyFrom(word);
-                    //dic.AddOrUpdate(key, word, (_, curVal) => curVal + " " + word);
-                }
+                var key = KeyFrom(word);
+                dic.AddOrUpdate(key, word, (_, currentValue) => currentValue + " " + word);
             });
-
-//            var parallelLoopResult = Parallel.ForEach(words, word =>
-//            {
-//                string ordered = new string(word.OrderBy(c => c).ToArray());
-//                dic.AddOrUpdate(ordered, word, (s, s1) => s + " " + s1);
-//                
-//            });
-
-//            foreach (var word in words.Where(x => x.Length > 1))
-//            {
-//                string ordered = new string(word.OrderBy(c => c).ToArray());
-//
-//                if (dic.ContainsKey(ordered))
-//                {
-//                    dic[ordered] += " " + word;
-//                }
-//                else
-//                {
-//                    dic[ordered] = word;
-//                }
-//            }
-
             return dic.Values.Where(x => x.Contains(" ")).ToArray();
         }
 
-//        private static int KeyFrom(string word)
-//        {
-//            
-//        }
+        private static int KeyFrom(string word)
+        {
+            //return a good hash here
+            return new Random().Next();
+        }
     }
 }
