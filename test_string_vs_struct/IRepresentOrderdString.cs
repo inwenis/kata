4334e43b889025a46fa2fc2c8d65794f0b17952c
Filip Kucharczyk <fku-ext@danskecommodities.com>
2018-09-29 09:55:42 +0200

    refactor + implementation changes

---
+++ test_string_vs_struct/IRepresentOrderdString.cs
@@ -0,0 +1,50 @@
+﻿namespace test_string_vs_struct
+{
+    public struct IRepresentOrderdString
+    {
+        public ulong one;
+        public ulong two;
+        public ulong three;
+
+        public override string ToString()
+        {
+            return $"{one:D21}{two:D21}{three:D21}";
+        }
+
+        public static bool operator ==(IRepresentOrderdString c1, IRepresentOrderdString c2)
+        {
+            return c1.one == c2.one && c1.two == c2.two && c1.three == c2.three;
+        }
+
+        public static bool operator !=(IRepresentOrderdString c1, IRepresentOrderdString c2)
+        {
+            return !(c1 == c2);
+        }
+
+        public static IRepresentOrderdString FromString(string word)
+        {
+            IRepresentOrderdString x = new IRepresentOrderdString();
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                var index = word[i] - 65;
+                if (index <= 19)
+                {
+                    x.one += Math.Pow(10, index);
+                }
+                else if(index <= (19 * 2))
+                {
+                    index -= 19;
+                    x.one += Math.Pow(10, index);
+                }
+                else
+                {
+                    index -= (19 * 2);
+                    x.three += Math.Pow(10, index);
+                }
+            }
+
+            return x;
+        }
+    }
+}
--- test_string_vs_struct/MathTests.cs
+++ test_string_vs_struct/MathTests.cs
@@ -5,11 +5,16 @@ using test_string_vs_struct;
 class MathTests
 {
     [Test]
-    [TestCase(10, 0, 1)]
-    [TestCase(10, 1, 10)]
-    [TestCase(10, 2, 100)]
-    [TestCase(10, 3, 1000)]
-    public void Test_Pow(int x, int y, int expected)
+    [TestCase(10, 0, 1UL)]
+    [TestCase(10, 1, 10UL)]
+    [TestCase(10, 2, 100UL)]
+    [TestCase(10, 3, 1000UL)]
+    [TestCase(10, 10, 10000000000UL)]
+    [TestCase(10, 15, 1000000000000000UL)]
+    [TestCase(10, 18, 1000000000000000000UL)]
+    [TestCase(10, 19, 10000000000000000000UL)]
+    //[TestCase(10, 20, 100000000000000000000UL)]
+    public void Test_Pow(int x, int y, ulong expected)
     {
         var pow = Math.Pow(x, y);
         Assert.AreEqual(expected, pow);
