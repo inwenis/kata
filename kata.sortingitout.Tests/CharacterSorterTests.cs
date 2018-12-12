45fac8a22e0eb5fb3229522d4ff54ce91ae49a1a
Filip Kucharczyk <fku-ext@danskecommodities.com>
2018-12-12 08:41:43 +0100

    handle sorting characters

---
+++ kata.sortingitout.Tests/CharacterSorterTests.cs
@@ -0,0 +1,40 @@
+﻿using kata.sortingitout;
+using NUnit.Framework;
+
+[TestFixture]
+class CharacterSorterTests
+{
+    [Test]
+    public static void Sort_EmptyString_ReturnsEmptyString()
+    {
+        // arrange
+
+        // act
+        var result = CharacterSorter.Sort("");
+
+        // assert
+        Assert.AreEqual("", result);
+    }
+
+    [Test]
+    public static void Sort_SortedText_ReturnsInput()
+    {
+        // arrange
+
+        // act
+        var result = CharacterSorter.Sort("aaabbb");
+
+        // assert
+        Assert.AreEqual("aaabbb", result);
+    }
+
+    [Test]
+    public static void Sort_UnsortedText_ReturnsSortedText()
+    {
+        // act
+        var result = CharacterSorter.Sort("bbbaaa");
+
+        // assert
+        Assert.AreEqual("aaabbb", result);
+    }
+}
---
+++ kata.sortingitout/CharacterSorter.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace kata.sortingitout
+{
+    public class CharacterSorter
+    {
+        public static string Sort(string input)
+        {
+            var characterCount = new Dictionary<char, int>();
+            for (char i = 'a'; i < 'z'; i++)
+            {
+                characterCount.Add(i, 0);
+            }
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                var character = input[i];
+                characterCount[character]++;
+            }
+
+            var builder = new StringBuilder(input.Length);
+            foreach (var keyValuePair in characterCount.OrderBy(x => x.Key))
+            {
+                var character = keyValuePair.Key;
+                var count = keyValuePair.Value;
+                builder.Append(character, count);
+            }
+            return builder.ToString();
+        }
+    }
+}
