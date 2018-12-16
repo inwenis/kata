2ba81d47576e906feff38ac3e392a6093473e921
Filip Kucharczyk <fku-ext@danskecommodities.com>
2018-12-16 17:16:43 +0100

    remove sample benchmark

--- kata.sortingitout.benchmark/Program.cs
+++ kata.sortingitout.benchmark/Program.cs
@@ -6,7 +6,6 @@ namespace MyBenchmarks
     {
         public static void Main(string[] args)
         {
-//            var summary = BenchmarkRunner.Run<Md5VsSha256>();
             var summary = BenchmarkRunner.Run<SortingBenchmark>();
         }
     }
