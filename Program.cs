824eb4eb1a3e3c99bd0ce1d57ad1c3ec1e4196cf
Filip Kucharczyk <fku-ext@danskecommodities.com>
2018-12-18 09:08:00 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace kata02.karate.chop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
