4c8ca31fc5b3f1d6be9c980e4dd51835abe18b4b
Filip Kucharczyk <fku-ext@danskecommodities.com>
2018-12-31 09:12:00 +0100

    check for single random word

--- app/Program.cs
+++ app/Program.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace app
 {
@@ -31,21 +33,41 @@ namespace app
             System.Console.WriteLine("Gimmie a word and I'll check if it is in the dictionary");
             System.Console.WriteLine("type `q!` (without the backticks) to exit");
             System.Console.WriteLine("type `d!` (without the backticks) to dump bitmap as binary");
-            string input;
-            do
+
+            TestUsingRandom5LetterWords(bm);
+
+            // string input;
+            // do
+            // {
+            //     input = System.Console.ReadLine();
+            //     if(input == "d!")
+            //     {
+            //         System.Console.WriteLine(bm.DumpBitMapToBinary());
+            //     }
+            //     else
+            //     {
+            //         System.Console.WriteLine($"Checking if {input} belongs to dictionary");
+            //         var check = bm.Check(input);
+            //         System.Console.WriteLine(check);
+            //     }
+            // }while(input != "q!");
+        }
+
+        private static void TestUsingRandom5LetterWords(BloomFilter bm)
+        {
+            var words = File.ReadAllLines("wordlist.txt");
+            Random random = new Random();
+            char[] chars = Enumerable
+                .Range(1, 5)
+                .Select(x => (char) random.Next('a', 'z'))
+                .ToArray();
+            string word = new string(chars);
+            System.Console.WriteLine(word);
+            if(bm.Check(word))
             {
-                input = System.Console.ReadLine();
-                if(input == "d!")
-                {
-                    System.Console.WriteLine(bm.DumpBitMapToBinary());
-                }
-                else
-                {
-                    System.Console.WriteLine($"Checking if {input} belongs to dictionary");
-                    var check = bm.Check(input);
-                    System.Console.WriteLine(check);
-                }
-            }while(input != "q!");
+                System.Console.WriteLine("BM says OK");
+                System.Console.WriteLine($"word in list: {words.Any(x => x == word)}");
+            }
         }
     }
 }
