9e8260c034eb5402dce544979f836e4b85115005
Filip Kucharczyk <fku-ext@danskecommodities.com>
2019-01-05 13:30:47 +0100

    print all sums

--- kata08.conflicting.objectives/spliter/Program.cs
+++ kata08.conflicting.objectives/spliter/Program.cs
@@ -10,7 +10,7 @@ namespace spliter
         {
             var words = File.ReadAllLines("wordlist.txt");
             var sums = FindSums.In(words);
-            foreach(var sum in sums.Take(10))
+            foreach(var sum in sums)
             {
                 Console.WriteLine($"{sum.Item1} + {sum.Item2} => {sum.Item3}");
             }
