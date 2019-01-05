c8090d7ba20930adce865c79acefba3492267313
Filip Kucharczyk <fku-ext@danskecommodities.com>
2019-01-05 12:13:57 +0100

    initial commit

---
+++ kata08.conflicting.objectives/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace spliter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
