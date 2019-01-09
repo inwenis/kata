6fb719655b8999cdd4c58d663bd148537be77fe9
Filip Kucharczyk <fku-ext@danskecommodities.com>
2019-01-09 08:28:31 +0100

    cleanup

--- kata08.conflicting.objectives/spliter/ProgramFast.cs
+++ kata08.conflicting.objectives/spliter/ProgramFast.cs
@@ -2,7 +2,6 @@ using System;
 using System.IO;
 using System.Linq;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Threading.Tasks;
 using System.Collections.Concurrent;
 using System.Text;
