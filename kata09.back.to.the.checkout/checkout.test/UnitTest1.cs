d8556e0ce3759910b9e3978733478adf1c1c6a3b
Filip Kucharczyk <fku-ext@danskecommodities.com>
2019-01-16 09:17:49 +0100

    test simple pricing

--- kata09.back.to.the.checkout/checkout.test/UnitTest1.cs
+++ kata09.back.to.the.checkout/checkout.test/UnitTest1.cs
@@ -1,4 +1,5 @@
 using checkout;
+using System.Collections.Generic;
 using NUnit.Framework;
 
 public class CheckoutTests
@@ -7,10 +8,26 @@ public class CheckoutTests
     public void ScanNoItems_TotalIs0()
     {
         // arrange
-        Checkout sut = new Checkout(null);
+        Checkout sut = new Checkout(new List<PricingRule>());
 
         // act
 
         Assert.AreEqual(0, sut.Total);
+    }
+
+    [Test]
+    public void ScanSingleItem_Monkey_TotalIsTotal()
+    {
+        // arrange
+        var rules = new List<PricingRule>()
+        {
+            new PricingRule('A', 50)
+        };
+        Checkout sut = new Checkout(rules);
+
+        // act
+        sut.Scan('A');
+
+        Assert.AreEqual(50, sut.Total);
     }
 }
--- kata09.back.to.the.checkout/checkout/Checkout.cs
+++ kata09.back.to.the.checkout/checkout/Checkout.cs
@@ -1,22 +1,37 @@
 using System.Collections.Generic;
+using System.Linq;
 
 namespace checkout
 {
     public class Checkout
     {
+        private PricingRule[] _pricingRules;
+        private decimal _total;
         public Checkout(IEnumerable<PricingRule> pricingRules)
         {
-
+            _pricingRules = pricingRules.ToArray();
         }
 
         public void Scan(char item)
         {
+            _total += _pricingRules.Single(x => x.Item == item).Price;
         }
 
-        public decimal Total => 0;
+        public decimal Total
+        {
+            get => _total;
+        }
     }
 
     public class PricingRule
     {
+        public readonly char Item;
+        public readonly decimal Price;
+
+        public PricingRule(char item, decimal price)
+        {
+            Item = item;
+            Price = price;
+        }
     }
 }
