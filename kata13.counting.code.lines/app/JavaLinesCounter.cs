6a306499cb508c3b52cc7f01f5ee905784ee21e9
Filip Kucharczyk <fku-ext@danskecommodities.com>
2019-02-12 08:16:06 +0100

    change to processing in loop - otherwise I could not get the latest test to pass

--- kata13.counting.code.lines/app/JavaLinesCounter.cs
+++ kata13.counting.code.lines/app/JavaLinesCounter.cs
@@ -8,9 +8,17 @@ public static class JavaLinesCounter
     // " // comment"
     // "\t// comment"
 
+    private static string MatchSingleLineBlockComments = @"/\*.*?\*/";
+    // Matches:
+    // "/*block comment*/
+
     private static string MatchBlockComments = @"/\*.*?\*/";
     // Matches:
     // "/*block comment \n more comment \n last line*/
+
+    private static string MatchBlockCommentOpening = @"/\*";
+    private static string MatchBlockCommentClosing = @"\*/";
+
     public static int Count(string code)
     {
         if(code.Length == 0)
@@ -18,19 +26,49 @@ public static class JavaLinesCounter
         else
         {
             string[] split = code.Split('\n');
-            int linesCount = split.Length;
-            int commentLines = split.Count(line => Regex.IsMatch(line, MatchSingleLineComments));
-            int emptyLines = split.Count(line => line.Trim() == "");
-            int multiLineComments = 0;
-
-            MatchCollection matches = Regex.Matches(code, MatchBlockComments, RegexOptions.Singleline);
-            if(matches.Any())
+            int linesCount = 0;
+            int singleLinesComments = 0;
+            int singleLinesBlockComments = 0;
+            int emptyLines = 0;
+            int blockCommentLines = 0;
+            bool blockCommentOpen = false;
+            foreach(string line in split)
             {
-                multiLineComments = matches
-                    .Sum(x => x.Value.Split('\n').Length);
-            }
+                if(blockCommentOpen && Regex.IsMatch(line, MatchBlockCommentClosing))
+                {
+                    blockCommentLines += 1;
+                    blockCommentOpen = false;
+                    continue;
+                }
+                else if(blockCommentOpen)
+                {
+                    blockCommentLines += 1;
+                    continue;
+                }
 
-            return linesCount - commentLines - emptyLines - multiLineComments;
+                if (Regex.IsMatch(line, MatchSingleLineComments))
+                {
+                    singleLinesComments += 1;
+                }
+                else if (line.Trim() == "")
+                {
+                    emptyLines += 1;
+                }
+                else if (Regex.IsMatch(line, MatchSingleLineBlockComments))
+                {
+                    singleLinesBlockComments += 1;
+                }
+                else if (Regex.IsMatch(line, MatchBlockCommentOpening))
+                {
+                    blockCommentLines += 1;
+                    blockCommentOpen = true;
+                }
+                else
+                {
+                    linesCount += 1;
+                }
+            }
+            return linesCount;
         }
     }
 }
--- kata13.counting.code.lines/test/JavaLinesCounterTests.cs
+++ kata13.counting.code.lines/test/JavaLinesCounterTests.cs
@@ -90,6 +90,23 @@ namespace Tests
         }
 
         [Test]
+        public void Count_SingleLineBlockComments_AreNotCountedAsCode()
+        {
+            string code = "" +
+            "/* this is a block comment */                                     " + "\n" +
+            "public class Hello {                                              " + "\n" +
+            "    public static final void main(String[] args) {                " + "\n" +
+            "        System.out.println(\"hello world\");                      " + "\n" +
+            "    }                                                             " + "\n" +
+            "}                                                                 ";
+
+            // act
+            int result = JavaLinesCounter.Count(code);
+            // assert
+            Assert.AreEqual(5, result);
+        }
+
+        [Test]
         public void Count_BlockComments_AreNotCountedAsCode()
         {
             string code = "" +
@@ -118,6 +135,25 @@ namespace Tests
             "here is ends */                                                   " + "\n" +
             "public class Hello {                                              " + "\n" +
             "/* another one */                                                 " + "\n" +
+            "    public static final void main(String[] args) {                " + "\n" +
+            "        System.out.println(\"hello world\");                      " + "\n" +
+            "    }                                                             " + "\n" +
+            "}                                                                 ";
+
+            // act
+            int result = JavaLinesCounter.Count(code);
+            // assert
+            Assert.AreEqual(5, result);
+        }
+
+        [Test]
+        public void Count_LineCommentInCommentBlock_IsNotSubtractedTwice()
+        {
+            string code = "" +
+            "/* this is a block comment                                        " + "\n" +
+            "//this line should not be subtracted twice                        " + "\n" +
+            "here is ends */                                                   " + "\n" +
+            "public class Hello {                                              " + "\n" +
             "    public static final void main(String[] args) {                " + "\n" +
             "        System.out.println(\"hello world\");                      " + "\n" +
             "    }                                                             " + "\n" +
