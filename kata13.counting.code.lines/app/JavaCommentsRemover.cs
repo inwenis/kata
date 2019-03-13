a964ff64ee9ea3007f2d80ef216873f8b978527e
Filip Kucharczyk <fku-ext@danskecommodities.com>
2019-03-13 09:17:14 +0100

    refactor - extract functions

--- kata13.counting.code.lines/app/JavaCommentsRemover.cs
+++ kata13.counting.code.lines/app/JavaCommentsRemover.cs
@@ -11,38 +11,57 @@ public static class JavaCommentsRemover
 
     public static string RemoveComments(string code)
     {
-        string codeWithReplacedStrings = code;
-        var strings = new List<KeyValuePair<string, string>>();
+        List<KeyValuePair<string, string>> strings = null;
+        string codeWithStringsReplaced = ReplaceStrings(code, ref strings);
+        string lineCommentsRemoved = RemoveLineComments(codeWithStringsReplaced);
+        string blockCommentsRemoved = RemoveBlockComments(lineCommentsRemoved);
+        string stringsReadded = ReAddStrings(blockCommentsRemoved, strings);
+        return stringsReadded;
+    }
+
+    private static string ReplaceStrings(string code, ref List<KeyValuePair<string, string>> replacedStrings)
+    {
+        replacedStrings = new List<KeyValuePair<string, string>>();
         var matches = Regex.Matches(code, MatchString);
-        foreach(var match in matches.Cast<Match>())
+        foreach (var match in matches.Cast<Match>())
         {
             string stringWithoutQuotes = match.Groups[1].Value;
             string id = Guid.NewGuid().ToString();
-            codeWithReplacedStrings = codeWithReplacedStrings.Replace(stringWithoutQuotes, id);
-            strings.Add(new KeyValuePair<string, string>(id, stringWithoutQuotes));
+            code = code.Replace(stringWithoutQuotes, id);
+            replacedStrings.Add(new KeyValuePair<string, string>(id, stringWithoutQuotes));
         }
+        return code;
+    }
 
-        string commentsRemoved = codeWithReplacedStrings;
-
-        matches = Regex.Matches(commentsRemoved, MatchLineComments, RegexOptions.Multiline);
-        foreach(var match in matches.Cast<Match>())
+    private static string RemoveLineComments(string code)
+    {
+        MatchCollection matches = Regex.Matches(code, MatchLineComments, RegexOptions.Multiline);
+        foreach (var match in matches.Cast<Match>())
         {
-            commentsRemoved = commentsRemoved.Replace(match.Groups[1].Value, "");
+            code = code.Replace(match.Groups[1].Value, "");
         }
+        return code;
+    }
 
-        matches = Regex.Matches(commentsRemoved, MatchBlockComments, RegexOptions.Singleline);
-        foreach(var match in matches.Cast<Match>())
+    private static string RemoveBlockComments(string code)
+    {
+        MatchCollection matches = Regex.Matches(code, MatchBlockComments, RegexOptions.Singleline);
+        foreach (var match in matches.Cast<Match>())
         {
             var newLinesCount = match.Groups[1].Value.Count(x => x == '\n');
             var replaceWith = string.Join("", Enumerable.Repeat('\n', newLinesCount));
-            commentsRemoved = commentsRemoved.Replace(match.Groups[1].Value, replaceWith);
+            code = code.Replace(match.Groups[1].Value, replaceWith);
         }
 
-        foreach(var item in strings)
+        return code;
+    }
+
+    private static string ReAddStrings(string code, List<KeyValuePair<string, string>> strings)
+    {
+        foreach (var item in strings)
         {
-            commentsRemoved = commentsRemoved.Replace(item.Key, item.Value);
+            code = code.Replace(item.Key, item.Value);
         }
-
-        return commentsRemoved;
+        return code;
     }
 }
