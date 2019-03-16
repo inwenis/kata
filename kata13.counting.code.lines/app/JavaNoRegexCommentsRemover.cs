3a1cc63e145acacf481575a9cb47ecb0d37d4bf3
Filip Kucharczyk <fku-ext@danskecommodities.com>
2019-03-16 23:27:48 +0100

    refactor - remove redundant code

--- kata13.counting.code.lines/app/JavaNoRegexCommentsRemover.cs
+++ kata13.counting.code.lines/app/JavaNoRegexCommentsRemover.cs
@@ -4,28 +4,21 @@ public class JavaNoRegexCommentsRemover
 {
     public static string RemoveComments(string code)
     {
-        if (code.Contains("//"))
+        StringBuilder output = new StringBuilder();
+        bool lineComment = false;
+        for (int i = 0; i < code.Length; i++)
         {
-            StringBuilder output = new StringBuilder();
-            bool lineComment = false;
-            for (int i = 0; i < code.Length; i++)
+            if(code[i] == '/' && code[i+1] == '/')
             {
-                if(code[i] == '/' && code[i+1] == '/')
-                {
-                    lineComment = true;
-                }
-
-                if (!lineComment)
-                {
-                    output.Append(code[i]);
-                }
+                lineComment = true;
             }
 
-            return output.ToString();
+            if (!lineComment)
+            {
+                output.Append(code[i]);
+            }
         }
-        else
-        {
-            return code;
-        }
+
+        return output.ToString();
     }
 }
