60397c3e5cd4080f439bbd0313f1977ef59a9d44
Filip Kucharczyk <fku-ext@danskecommodities.com>
2019-03-17 00:41:38 +0100

    add comment

--- kata13.counting.code.lines/app/JavaLinesCounter.cs
+++ kata13.counting.code.lines/app/JavaLinesCounter.cs
@@ -6,6 +6,7 @@ public static class JavaLinesCounter
     public static int Count(string code)
     {
         string codeWithoutComments = JavaCommentsRemover.RemoveComments(code);
+        //string codeWithoutComments = JavaNoRegexCommentsRemover.RemoveComments(code); // uncomment this line to use the other CommentsRemover
         string[] split = codeWithoutComments.Split('\n');
         int loc = split.Count(line => line.Trim() != "");
         return loc;
