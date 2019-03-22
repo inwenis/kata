242ca5359ec1fe2973c140f1febf0cc516505905
Filip Kucharczyk <fku-ext@danskecommodities.com>
2019-03-22 08:20:10 +0100

    add unit tests

---
+++ kata14.trigrams/tests/TrigramParserTests.cs
@@ -0,0 +1,15 @@
+using NUnit.Framework;
+
+public class Tests
+{
+    [Test]
+    public void WarmUpTest()
+    {
+        // arrange
+
+        // act
+
+        // assert
+        Assert.IsTrue(true);
+    }
+}
