d5bedfd1ae1d2fe754db8b9ab9c5ea9a72324c0e
Filip Kucharczyk <fku-ext@danskecommodities.com>
2019-03-22 08:30:39 +0100

    add warmup test

--- kata14.trigrams/tests/TrigramParserTests.cs
+++ kata14.trigrams/tests/TrigramParserTests.cs
@@ -1,15 +1,18 @@
+using System.Collections.Generic;
 using NUnit.Framework;
 
 public class Tests
 {
     [Test]
-    public void WarmUpTest()
+    public void Parse_EmptyInput_ReturnsEmptyDictionary()
     {
         // arrange
 
         // act
+        Dictionary<(string, string), string> result = TrigramParser.Parse("");
 
         // assert
-        Assert.IsTrue(true);
+        Assert.IsNotNull(result);
+        Assert.IsEmpty(result);
     }
 }
