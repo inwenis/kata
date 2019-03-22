31b32fbde758d1ef0a634ccf6e8c39dd8eb8e97b
Filip Kucharczyk <fku-ext@danskecommodities.com>
2019-03-22 08:52:20 +0100

    refactor

--- kata14.trigrams/app/TrigramParser.cs
+++ kata14.trigrams/app/TrigramParser.cs
@@ -5,34 +5,12 @@ public class TrigramParser
 {
     public static System.Collections.Generic.Dictionary<(string, string), string> Parse(string text)
     {
-        if (text == "")
-        {
-            return new Dictionary<(string, string), string>();
-        }
         string[] split = text.Split(" ");
-        if (split.Length == 3)
+        var dic = new Dictionary<(string, string), string>();
+        for( int i = 0; i < split.Length - 2; ++i)
         {
-            return new Dictionary<(string, string), string>
-            {
-                {(split[0], split[1]), split[2]}
-            };
+            dic.Add((split[i], split[i+1]), split[i+2]);
         }
-        else if (split.Length == 4)
-        {
-            return new Dictionary<(string, string), string>
-            {
-                {(split[0], split[1]), split[2]},
-                {(split[1], split[2]), split[3]}
-            };
-        }
-        else
-        {
-            var dic = new Dictionary<(string, string), string>();
-            for( int i = 0; i < split.Length - 2; ++i)
-            {
-                dic.Add((split[i], split[i+1]), split[i+2]);
-            }
-            return dic;
-        }
+        return dic;
     }
 }
