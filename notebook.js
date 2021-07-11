9f977d52dacdc30a8180c9262ffbbd30086e946b
Filip Kucharczyk <fil.kucharczyk@gmail.com>
2021-07-11 13:59:57 +0200

    add notebook

---
+++ notebook.js
@@ -0,0 +1,41 @@
+console.log(typeof 42)
+console.log(typeof 42.1)
+console.log(typeof "42.1")
+console.log(typeof '42.1')
+console.log(typeof `42.1`)
+console.log(2.988e10)
+console.log(29.88e10)
+console.log(1/0)
+console.log(0/1)
+console.log(0/0)
+console.log(NaN === 0/0) // returns false
+console.log(NaN)
+let a = NaN
+let b = NaN
+console.log(a === b) // that's false
+let str1 = "aaa"
+let str2 = `this is a 
+
+new lines in a string
+
+
+test`
+console.log(str2)
+let x =
+
+"aaa"
+console.log(x)
+console.log(`this is a string template literal ${100/3}`)
+
+// Boolean
+console.log(true)
+console.log(typeof true)
+console.log(typeof (3 > 2))
+console.log(3 > 2)
+
+console.log("a" > "b")
+console.log("x" > "b")
+console.log("X" > "x")
+
+console.log("Itchy" != "Scratchy") // → true
+console.log("Apple" == "Orange") // → false
