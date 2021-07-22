badd275216da0f6c75005a3c6f72d11c6a6d05eb
Filip Kucharczyk <fil.kucharczyk@gmail.com>
2021-07-22 22:52:49 +0200

    user story 2

--- 06_bank_ocr.js
+++ 06_bank_ocr.js
@@ -23,16 +23,27 @@ function decode(test1) {
     let t0, t1, t2, t3
     [t0, t1, t2, t3] = test1.split('\n')
 
-    let result = '';
+    const result = [];
     for (let i = 0; i < 9; i++) {
         const d =
             t1.substring(i*3, (i+1)*3) + '\n' +
             t2.substring(i*3, (i+1)*3) + '\n' +
             t3.substring(i*3, (i+1)*3)
-        result += dict[d];
+        result.push(dict[d]);
     }
 
     return result;
+}
+
+function validateCheckSum(digits) {
+    //checksum calculation:
+    //(d1+2*d2+3*d3+...+9*d9) mod 11 = 0
+    let sum = 0;
+    for (let i = 0; i < 9; i++) {
+        sum += digits[i] * (i+1)
+    }
+    const isValid = sum % 11 === 0;
+    return isValid;
 }
 
 let test1 =
@@ -55,7 +66,19 @@ let test3 =
 | | _| _||_||_ |_   ||_||_|
 |_||_  _|  | _||_|  ||_| _|
 `
+let test4 =
+`
+ _  _  _  _  _  _  _  _  _ 
+| || || || || || || || || |
+|_||_||_||_||_||_||_||_||_|
+`
 
 console.log(decode(test1))
 console.log(decode(test2))
 console.log(decode(test3))
+
+// user story 2
+console.log(validateCheckSum(decode(test1)))
+console.log(validateCheckSum(decode(test2)))
+console.log(validateCheckSum(decode(test3)))
+console.log(validateCheckSum(decode(test4)))
